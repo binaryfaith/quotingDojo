37a1d3501c8de9afb7e6ba5052be51392f4819d8
binaryfaith <binaryfaith@gmail.com>
2017-12-12 16:21:21 -0800

    fully modularized

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express'),
 bodyParser = require('body-parser'),
-mongoose = require('mongoose'),
 path = require('path'),
 port = 8000,
 app = express();
@@ -8,24 +7,12 @@ app = express();
 // Set up body-parser to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up database connection, Schema, model
-mongoose.connect('mongodb://localhost/quoting_dojo');
-
-const quoteSchema = new mongoose.Schema({
-name: String,
-quote: String
-});
-
-const Quote = mongoose.model('quotes', quoteSchema);
-
 // Point server to views
 app.set('views', path.join(__dirname, 'views'));
 // We're using ejs as our view engine
 app.set('view engine', 'ejs');
-
+require('./server/config/mongoose.js');
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
-
-
 
 app.listen(port);
